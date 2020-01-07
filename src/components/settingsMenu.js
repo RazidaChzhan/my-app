d2fe5f9b96d37b3eb192e1eb6467f2f0babd2caa
RazidaChzhan <chrazida@gmail.com>
2020-01-07 21:45:18 +0600

    Работают ссылки меню Основная информация/Социальные сети

--- src/components/settingsMenu.js
+++ src/components/settingsMenu.js
@@ -1,31 +1,92 @@
 import React from "react";
 import { Typography,  Grid } from '@material-ui/core';
 
-import Leftmenu from './leftMenu.js';
+// import Leftmenu from './leftMenu.js';
 import Profilemenu from './profileMenu.js';
 import SocialNetworks from './socialnetworks.js';
 
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+
+import {
+    HashRouter,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
 import '../index.css';
 
+// export default class SettingsMenu extends React.Component {
+//     render () {
+//         return (
+//             <div>
+//                 <Typography className="backgroundimage">
+//                     <Grid container spacing={1}>
+//                         <Grid item xs={1}></Grid>
+//                         <Grid item xs={2} 
+//                         style={{backgroundColor: '#FFFFFF', marginTop: '80px', borderRadius: '5px', padding: '25px', width: '441px', height: '186px'}}>
+//                             <Leftmenu/>   
+//                         </Grid>
+//                         <Grid item xs={6}
+//                         style={{backgroundColor: '#ffffff', marginTop: '80px', marginLeft: '31px', borderRadius: '5px'}}>
+//                             <Profilemenu/>                             
+//                             <SocialNetworks/>
+//                         </Grid>
+//                     </Grid>   
+//                 </Typography> 
+//             </div>
+//         );
+//     }
+// } 
+
 export default class SettingsMenu extends React.Component {
-    render () {
-        return (
-            <div>
-                <Typography className="backgroundimage">
-                    <Grid container spacing={1}>
-                        <Grid item xs={1}></Grid>
-                        <Grid item xs={2} 
-                        style={{backgroundColor: '#FFFFFF', marginTop: '80px', borderRadius: '5px', padding: '25px', width: '441px', height: '186px'}}>
-                            <Leftmenu/>   
-                        </Grid>
-                        <Grid item xs={6}
-                        style={{backgroundColor: '#ffffff', marginTop: '80px', marginLeft: '31px', borderRadius: '5px'}}>
-                            <Profilemenu/>                             
-                            {/* <SocialNetworks/> */}
-                        </Grid>
-                    </Grid>   
-                </Typography> 
-            </div>
-        );
-    }
-} 
+        render () {
+            return (
+                <HashRouter>      
+               <Typography className="backgroundimage">
+                        <Grid container spacing={1}>
+                            <Grid item xs={1}></Grid>
+                            <Grid item xs={2} 
+                            style={{backgroundColor: '#FFFFFF', marginTop: '80px', borderRadius: '5px', padding: '25px', width: '441px', height: '186px'}}>
+                                    
+                    <MenuList  >
+                        <Link className="linktext1" to="/main">
+                            <MenuItem className="menuitemtext" style={{borderBottom: '2px solid #D2D2D2', paddingBottom: 10}}>
+                            Основная информация
+                            </MenuItem>
+                        </Link>
+                        <Link className="linktext1" to="/socialnetworks">
+                            <MenuItem className="menuitemtext" style={{borderBottom: '2px solid #D2D2D2', paddingBottom: 10}}>
+                            Социальные сети
+                            </MenuItem>
+                        </Link>
+                        <Link className="linktext1" to="/changepassword">
+                            <MenuItem className="menuitemtext" style={{paddingTop: 10}}>
+                            Смена пароля
+                            </MenuItem>
+                        </Link>                  
+                    </MenuList>
+                   
+                            </Grid>
+                            <Grid item xs={6}
+                            style={{backgroundColor: '#ffffff', marginTop: '80px', marginLeft: '31px', borderRadius: '5px'}}>
+                              
+                                <Switch>
+                                <Route path="/main" component={Profilemenu}>
+                                </Route>
+                                <Route path="/socialnetworks" component={SocialNetworks}>
+                                
+                                </Route>
+                                <Route path="/changepassword">
+                                <h1>changepassword</h1>
+                                </Route>
+                                </Switch>
+                                
+                            </Grid>
+                        </Grid>   
+                    </Typography> 
+                </HashRouter> 
+            );
+        }
+    } 
--- src/components/socialnetworks.js
+++ src/components/socialnetworks.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Grid, Icon } from '@material-ui/core';
-import SvgIcon from '@material-ui/core/SvgIcon';
-import VkIcon from './vk.svg';
+// import SvgIcon from '@material-ui/core/SvgIcon';
+// import VkIcon from './vk.svg';
+import logovkontaktegrey from '../images/logovkontaktegrey.svg';
+import logofacebookgrey from '../images/logofacebookgrey.svg';
+import logoinstagramgrey from '../images/logoinstagramgrey.svg'
 
 
-
-import FacebookIcon from '@material-ui/icons/Facebook';
-import InstagramIcon from '@material-ui/icons/Instagram';
+// import FacebookIcon from '@material-ui/icons/Facebook';
+// import InstagramIcon from '@material-ui/icons/Instagram';
 
 import '../index.css';
 
@@ -21,14 +23,16 @@ export default class SocialNetworks extends React.Component {
                     <hr></hr>
                     <Grid container spacing={1}>
                         <Grid item xs={3}>
-                            <FacebookIcon color="disabled"/> Facebook
+                            {/* <FacebookIcon color="disabled"/> Facebook */}
+                            <img src={logofacebookgrey} alt={logofacebookgrey} style={{width: 30, height: 30}}></img> Facebook
                         </Grid>
                         <Grid item xs={3}>
-                        
+                        <img src={logovkontaktegrey} alt={logovkontaktegrey} style={{width: 30, height: 30}}></img> ВКонтакте
                            
                         </Grid>
                         <Grid item xs={3}>
-                        <InstagramIcon color="disabled"/>
+                        {/* <InstagramIcon color="disabled"/> instagram */}
+                        <img className="logoinstagram" src={logoinstagramgrey} alt={logoinstagramgrey}></img> instagram
                         </Grid>
                     </Grid>
                 </Grid>
