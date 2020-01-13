44f2871b3019a60afa3572b015940523605ff3bf
RazidaChzhan <chrazida@gmail.com>
2020-01-13 21:20:57 +0600

    Откоррректирован код header.js, footer.js, страница Профиля

--- src/components/footer.js
+++ src/components/footer.js
@@ -35,54 +35,54 @@ const useStyles = makeStyles(theme => ({
       <div className={classes.root}>
         <AppBar 
         position="static"
-        style={{background: '#F0F3F4' }}>
+        style={{background: '#F0F3F4', paddingTop: 35, paddingBottom: 25}}>
           <Toolbar>
-            <Typography variant="h6" className={classes.title}>
+            <Typography className={classes.title}>
                 <Grid container spacing={1}>
                 <Grid item xs={1}></Grid>
-                    <Grid item xs={5}>
+                    <Grid item xs={5} style={{paddingLeft: 80}}>
                             <Grid container spacing={1}>
-                                <Grid item xs={3}>
+                                <Grid item xs={2}>
                                 <img className="headerimg" src={logoheader} alt={logoheader} style={{width: 114, height: 85}}></img>
                                 </Grid>
-                                <Grid item xs={4}>
+                                <Grid item xs={4}  style={{marginLeft: 30, borderLeft: '3px solid #2FA2BC', height: 64, marginTop: 22}}>
                                 <p className="headertext">Образовательная <br></br>онлайн-платформа</p>
                                 </Grid>
                             </Grid>
 
                             <Grid container spacing={1}>
-                                <Grid item xs={3} style={{marginLeft: 30}}>
+                                <Grid item xs={3} style={{marginLeft: 20, paddingTop: 20}}>
                                 <p className="footerphone">8 800 000-00-00</p>
                                 <p className="footertext">Бесплатно по России</p>
                                 </Grid>
-                                <Grid item xs={3}>
+                                <Grid item xs={3} style={{marginLeft: 20, paddingTop: 20}}>
                                 <p className="footerphone">+7 3452 00-00-00</p>
                                 <p className="footertext">Тюмень и область</p>   
                                 </Grid>
                             </Grid>
                         </Grid>
-                        <Grid item xs={3}>
+                        <Grid item xs={3} style={{paddingLeft: 90}}>
                             <p className="footertext">Договор оферты</p>
                             <a className="policy">Политика конфидинциальности</a>
                             <p className="footertext">мойуниверситет.рф <CopyrightSharpIcon fontSize="small"/> 2019</p>
                             <Grid container spacing={1}>
                                 <Grid item xs={1}>
-                                    <img src={logovkontakte} alt={logovkontakte}></img>
+                                    <img src={logovkontakte} alt={logovkontakte} style={{width: 40, height: 40}}></img>
                                 </Grid>
                                 <Grid item xs={1}>
-                                    <img src={logoinstagram} alt={logoinstagram}></img>
+                                    <img src={logoinstagram} alt={logoinstagram} style={{width: 40, height: 40, paddingLeft: 20}}></img>
                                 </Grid>
                             </Grid>
                         </Grid>
-                        <Grid item xs={3}>
+                        <Grid item xs={3} style={{paddingLeft: 50}}>
                             <Grid container spacing={1}>
-                                <Grid item xs={5} style={{paddingTop: 30}}>
+                                <Grid item xs={5} style={{paddingTop: 10}}>
                                     <div className="footertext" style={{paddingTop: '25px'}}>
                                       Разработка сайта 
                                       <br></br>Менделеев Маркетинг
                                       </div>
                                 </Grid>
-                                <Grid item xs={4} style={{paddingTop: 44}}>
+                                <Grid item xs={4} style={{paddingTop: 20, paddingLeft: 20}}>
                                     <img src={footerlogo} alt={footerlogo}></img>
                                 </Grid>
                             </Grid>
--- src/components/mainmenu1.js
+++ src/components/mainmenu1.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
-import { Grid } from '@material-ui/core';
+import { Grid, Button } from '@material-ui/core';
 
 import avatar from '../images/avatar.png';
 import rightarrow from '../images/right-arrow.png'
@@ -22,98 +22,115 @@ export default function AppMainMenu() {
      return (
    
     <ThemeProvider theme={theme}>
-        <Typography className="backgroundimage">
-        <Grid container>
-          <Grid item xs={1} style={{paddingLeft: 270}}></Grid>
-          <Grid item xs={3} style={{backgroundColor: '#FFFFFF', marginTop: 85, borderRadius: 5, padding: 25, width: 405, height: 425}}>
-            <Grid container spacing={10}>
-                <Grid item xs={3}>
-                <img src={avatar} alt={avatar} style={{width: 64, height: 64, paddingLeft: 27, paddingTop: '10px'}}></img>   
-                </Grid>
-                <Grid item xs={8}>
-                <p><b>Иван Михайлов</b></p>
-                <p className="footertext">студент</p>   
-                </Grid>
-            </Grid>
-                <hr></hr>
-                <Grid item={10} style={{paddingTop: 12, paddingLeft: 27, paddingBottom: 8}}>
-                    <p>web.d3v@yandex.ru</p>
-                    <p>+7 952 347-34-19</p>
-                    <p><b>Тюмень</b></p>
-                </Grid>
-                <hr></hr>
-                <Grid item={10} style={{paddingTop: 3, paddingLeft: 27}}>
-                <p>Вы прошли: <b>0 треков</b></p>
-                <p>Активный трек: <b>1</b></p>
-                </Grid>
-        </Grid> 
-        <Grid item xs={6} style={{marginTop: '65px'}}>
-        <Grid container spacing={1} style={{marginLeft: '20px', marginBottom: '30px'}}>
-            <Grid item xs={1}>
-            <a className="linkbutton">Все</a>
-            </Grid>
-            <Grid item xs={2}>
-            <a className="linkbutton">Мои треки</a>
-            </Grid>
-            <Grid item xs={1}>
-            <a className="linkbutton">Пройденные</a>
-            </Grid>
-        </Grid>
+        <Typography className="backgroundimage" style={{paddingBottom: 225}}>
+            <Grid container>
+                <Grid item xs={1}></Grid>
 
-        <Grid container spacing={1}>
-            <Grid item xs={4}>
-                <span className="linktext">Социальное проектирование</span>
-            </Grid>
-            <Grid item xs={3}>
-                <span className="linktext">Программирование</span>
-            </Grid>
-            <Grid item xs={2}>
-                <span className="linktext">Веб-дизайн</span>
-            </Grid>
-            <Grid item xs={3}>
-                <span className="linktext">Английский язык</span>
-            </Grid>
-        </Grid>
-        <Grid container spacing={1} style={{backgroundColor: '#FFFFFF', marginLeft: 20, marginTop: 20, height: 90, paddingTop: 15,  width: 927, height: 120}}>
-            <Grid item xs={1}></Grid>
-            <Grid item xs={5} style={{paddingTop: 20}}>
-            <span><b>Социальное проектирование</b></span>
-            </Grid>
-            <Grid item xs={4} style={{paddingTop: '20px'}}>
-            <span className="track">Трек пройден на 0%</span>   
-            </Grid>
-            <Grid item xs={2}>
-            <img src={rightarrow} alt={rightarrow} style={{width: '50px', height: '50px'}}></img>  
-            </Grid>
-        </Grid>
+                <Grid item xs={3} style={{backgroundColor: '#FFFFFF', marginTop: 85, borderRadius: 5, padding: 25, width: 405, height: 425, marginLeft: 110}}>
+                    <Grid container spacing={10}>
+                    <Grid item xs={3}>
+                        <img src={avatar} alt={avatar} style={{width: 64, height: 64, paddingLeft: 27, paddingTop: '10px'}}></img>   
+                    </Grid>
+                    <Grid item xs={8}>
+                        <p><b>Иван Михайлов</b></p>
+                        <p className="footertext">студент</p>   
+                    </Grid>                    
+                    </Grid> 
+                    <hr style={{width: 360}}></hr>
+                    <Grid item={10} style={{paddingTop: 12, paddingLeft: 27, paddingBottom: 8}}>
+                        <p>web.d3v@yandex.ru</p>
+                        <p>+7 952 347-34-19</p>
+                        <p><b>Тюмень</b></p>
+                    </Grid>
+                    <hr style={{width: 360}}></hr>
+                    <Grid item={10} style={{paddingTop: 3, paddingLeft: 27}}>
+                        <p>Вы прошли: <b>0 треков</b></p>
+                        <p>Активный трек: <b>1</b></p>
+                    </Grid>
+                </Grid>
 
-        <Grid container spacing={1} style={{backgroundColor: '#FFFFFF', marginLeft: '20px', marginTop: '20px', height: '90px', paddingTop: '15px'}}>
-            <Grid item xs={1}></Grid>
-            <Grid item xs={5} style={{paddingTop: '20px'}}>
-            <span><b>Введение в веб-дизайн</b></span>
-            </Grid>
-            <Grid item xs={4} style={{paddingTop: '20px'}}>
-            <span className="track">Трек пройден на 0%</span>   
-            </Grid>
-            <Grid item xs={2}>
-            <img src={rightarrow} alt={rightarrow} style={{width: '50px', height: '50px'}}></img>  
-            </Grid>
-        </Grid>
+                <Grid item xs={7}>
+                    <Grid container spacing={1} style={{ marginBottom: 30, marginTop: 60}}>
+                        <Grid item xs={1}>
+                            <Button className="linkbutton" 
+                            style={{ marginLeft: 40, textTransform: 'capitalize', fontFamily: 'Segoe UI', fontSize: 24, color: '#222222'}}>
+                            Все</Button>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Button className="linkbutton"
+                            style={{textTransform: 'capitalize', fontFamily: 'Segoe UI', fontSize: 24, color: '#222222'}}>
+                            Мои треки</Button>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <Button className="linkbuttonpassed"
+                            style={{textTransform: 'capitalize', fontFamily: 'Segoe UI', fontSize: 24, color: '#222222'}}
+                            >Пройденные</Button>
+                        </Grid>
+                    </Grid>     
 
-        <Grid container spacing={1} style={{backgroundColor: '#FFFFFF', marginLeft: '20px', marginTop: '20px', height: '90px', paddingTop: '15px'}}>
-            <Grid item xs={1}></Grid>
-            <Grid item xs={5} style={{paddingTop: '20px'}}>
-            <span><b>Английский для IT-специалистов</b></span>
+                    <Grid container>
+                        <Grid item xs={3}>
+                            <span className="linktext">Социальное проектирование</span>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <span className="linktext">Программирование</span>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <span className="linktext">Веб-дизайн</span>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <span className="linktext" style={{marginLeft: 0}}>Английский язык</span>
+                        </Grid>
+                    </Grid>
+
+                    <Grid container>
+                        <Grid container spacing={1} style={{backgroundColor: '#FFFFFF', marginLeft: 30, marginTop: 40, height: 90, paddingTop: 20,  paddingLeft: 50, width: 990, height: 130}}>
+                           
+                            <Grid item xs={7}>
+                                <h3 className="textall">Социальное проектирование</h3>
+                            </Grid>
+                            <Grid item xs={3} style={{paddingTop: 40, paddingLeft: 25}} >
+                                <span className="track">Трек пройден на 0%</span>   
+                            </Grid>
+                            <Grid item xs={2} style={{paddingTop: 20, paddingLeft: 50}}>
+                                <img src={rightarrow} alt={rightarrow} style={{width: 60, height: 60}}></img>  
+                            </Grid>
+                        </Grid>
+                    </Grid>
+
+                    <Grid container>
+                        <Grid container spacing={1} style={{backgroundColor: '#FFFFFF', marginLeft: 30, marginTop: 25, height: 90, paddingTop: 15, paddingLeft: 50,   width: 990, height: 130}}>
+                           
+                            <Grid item xs={7}>
+                                <h3 className="textall">Введение в веб-дизайн</h3>
+                            </Grid>
+                            <Grid item xs={3} style={{paddingTop: 40, paddingLeft: 25}}>
+                                <span className="track" >Трек пройден на 0%</span>   
+                            </Grid>
+                            <Grid item xs={1} style={{paddingTop: 20, paddingLeft: 50}}>
+                                <img src={rightarrow} alt={rightarrow} style={{width: 60, height: 60}}></img>  
+                            </Grid>
+                        </Grid>
+                    </Grid>
+
+                    <Grid container>
+                        <Grid container spacing={1} style={{backgroundColor: '#FFFFFF', marginLeft: 30, marginTop: 25, height: 90, paddingTop: 15,  paddingLeft: 50, width: 990, height: 130}}>
+                            
+                            <Grid item xs={7}>
+                            <h3 className="textall">Английский для IT-специалистов</h3>
+                            </Grid>
+                            <Grid item xs={3}style={{paddingTop: 40,paddingLeft: 25 }}>
+                                <span className="track">Трек пройден на 0%</span>   
+                            </Grid>
+                            <Grid item xs={2} style={{paddingTop: 20, paddingLeft: 50}}>
+                                <img src={rightarrow} alt={rightarrow} style={{width: 60, height: 60}}></img>  
+                            </Grid>
+                        </Grid>
+                    </Grid>
+
+
+                </Grid>                    
             </Grid>
-            <Grid item xs={4} style={{paddingTop: '20px'}}>
-            <span className="track">Трек пройден на 0%</span>   
-            </Grid>
-            <Grid item xs={2}>
-            <img src={rightarrow} alt={rightarrow} style={{width: '50px', height: '50px'}}></img>  
-            </Grid>
-        </Grid>
-        </Grid>
-        </Grid>            
         </Typography>
      </ThemeProvider>   
   );
